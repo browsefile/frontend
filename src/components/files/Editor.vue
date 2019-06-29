153beab955ebffdd53d010e00c7ae12282efefef
Vitaliy <vitaliy.kuzmich@gmail.com>
2019-06-29 12:25:04 +0300

    fix text editor

--- src/components/files/Editor.vue
+++ src/components/files/Editor.vue
@@ -1,5 +1,6 @@
 <template>
-    <form id="editor" :class="getLang">
+    <form>
+        <textarea id="editor"></textarea>
     </form>
 </template>
 
@@ -7,48 +8,15 @@
     import {mapState} from 'vuex'
     import {files as api} from '@/api'
     import buttons from '@/utils/buttons'
-    import CodeMirror from '@/utils/codemirror-setup'
 
     export default {
         name: 'editor',
         computed: {
             ...mapState(['req']),
-            getLang() {
-                let mode = ""
-                switch (this.req.extension) {
-                    case 'md', 'markdown', 'mdown', 'mmark':
-                        mode = 'markdown'
-                        break
-                    case 'yml':
-                        mode = 'yaml'
-                        break
-                    case 'asciidoc', 'adoc', 'ad':
-                        mode = 'asciidoc'
-                        break
-                    case 'rst':
-                        mode = 'rst'
-                        break
-                    case 'html', 'htm', 'xml':
-                        mode = 'htmlmixed'
-                        break
-                    case 'js':
-                        mode = 'javascript'
-                        break
-                    case 'go':
-                        mode = 'golang'
-                        break
-                    case '':
-                        mode = 'text'
-                        break
-                }
-                return mode;
-            }
+
         },
         data: function () {
             return {
-                metadata: null,
-                metalang: null,
-                editor: null
             }
         },
         created() {
@@ -59,25 +27,26 @@
             window.removeEventListener('keydown', this.keyEvent)
             document.getElementById('save-button').removeEventListener('click', this.save)
         },
-        mounted () {
+        mounted() {
             if (this.req.content === undefined || this.req.content === null) {
                 this.req.content = ''
             }
             if (this.req.extension) {
                 this.req.language = this.getLang;
             }
+            var editor = document.getElementById('editor')
+            editor.value = this.req.content
+            editor.addEventListener('input', ()=>autoresize(editor), false);
 
-            this.editor = CodeMirror(document.getElementById('editor'), {
-                value: this.req.content,
-                lineNumbers: (this.req.language !== 'markdown'),
-                viewportMargin: 500,
-                autofocus: true,
-                mode: this.req.language,
-                theme: (this.req.language === 'markdown') ? 'markdown' : 'ttcn',
-                lineWrapping: (this.req.language === 'markdown')
-            })
-            CodeMirror.autoLoadMode(this.editor, 'content')
 
+            function autoresize(editor) {
+                editor.style.height = 'auto'
+                editor.style.height = editor.scrollHeight + 'px'
+                editor.scrollTop = editor.scrollHeight
+                window.scrollTo(window.scrollLeft, (editor.scrollTop + editor.scrollHeight))
+            }
+
+            autoresize(editor)
         },
         methods: {
             keyEvent(event) {
@@ -95,10 +64,8 @@
             async save() {
                 const button = 'save'
                 buttons.loading('save')
-                let content = this.editor.getValue()
-                if (this.hasMetadata) {
-                    content = this.metadata.getValue() + '\n\n' + content
-                }
+                let content = document.getElementById("editor").value
+
                 try {
                     await api.put(this.$route.path, content)
                     buttons.success(button)
