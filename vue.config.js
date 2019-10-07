f59c52e57d70385f4c3700fff855dbebb02d4b38
Vitaliy <vitaliy.kuzmich@gmail.com>
2019-10-07 20:39:32 +0300

    upd last request during search

--- vue.config.js
+++ vue.config.js
@@ -8,27 +8,35 @@ module.exports = {
                 apply: (compiler) => {
                     const exec = require('child_process').exec
                     const fs = require('fs')
-                    compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-                        let p = arguments[4] + '/dist/index.html'
-                        console.dir('Apply patch to ' + p)
+                    let basePath = arguments[4]
+
+                    compiler.plugin("done", function () {
+                        let p = basePath + '/dist/index.html'
+
                         fs.readFile(p, 'utf8', function (err, data) {
                             if (err) {
                                 return console.log(err);
                             }
                             let result = data.replace(/\/\[/g, '[')
-                            fs.writeFile(p, result, 'utf8', function (err) {
-                                if (err) return console.log(err);
-                            });
-                        })
-                        p = arguments[4].split('/').slice(0, -1).join('/') + "/src/lib/"
+                            setTimeout(() => {
+                                console.dir('Apply patch to ' + p)
+                                fs.writeFile(p, result, 'utf8', (err) => {
+                                    if (err) return console.log(err)
+                                })
+                                p = basePath.split('/').slice(0, -1).join('/') + "/src/lib/"
 
-                        //have to install go rice tool before use this
-                        exec('rm rice-box.go; rice embed-go', {cwd: p, shell: '/bin/sh', env: process.env},
-                            (err, stdout, stderr) => {
-                                console.dir('ricebox gen done at ' + p)
-                                if (stdout) process.stdout.write(stdout);
-                                if (stderr) process.stderr.write(stderr);
-                            });
+                                //have to install go rice tool before use this
+                                exec('rm rice-box.go; rice embed-go', {cwd: p, shell: '/bin/sh', env: process.env},
+                                    (err, stdout, stderr) => {
+                                        console.dir('ricebox gen done at ' + p)
+                                        if (stdout) process.stdout.write(stdout)
+                                        if (stderr) process.stderr.write(stderr)
+                                    })
+                            }, 100)
+
+                        })
+
+
                     });
                 }
             }
